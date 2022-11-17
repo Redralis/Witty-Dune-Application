e3c0915bea2c326d0d9950a72eabade9cbc37884
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-11-17 14:29:19 +0100

    Initial push

---
+++ apps/witty-dune-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'witty-dune-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'witty-dune-app';
+}
